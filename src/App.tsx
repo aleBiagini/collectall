c23ab2fb31d3af84ab5d9b8b79162a279ec89437
abiagini <4Fp4r5WHBw1E>
2023-10-03 21:31:29 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import PrimarySearchAppBar from './components/PrimarySearchAppBar'
+
+function App() {
+
+  return (
+    <PrimarySearchAppBar></PrimarySearchAppBar>
+  )
+}
+
+export default App
