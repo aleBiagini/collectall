a109cade2b98af1c8a11556b571723b18bf5d60b
dbrignoli <davide.brignoli@yourbiz.it>
2023-10-04 13:00:08 +0200

    Insert API for getting all cards from a set

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import MultipleSelectPlaceholder from "./components/MultipleSelectPlaceholder/MultipleSelectPlaceholder";
 import ResponsiveAppBar from "./components/ResponsiveAppBar";
-import StandardImageList from "./components/StandardImageList/StandardImageList";
 
 function App() {
   return (
     <>
       <ResponsiveAppBar></ResponsiveAppBar>
       <MultipleSelectPlaceholder></MultipleSelectPlaceholder>
-      <StandardImageList></StandardImageList>
     </>
   );
 }
--- src/components/MultipleSelectPlaceholder/MultipleSelectPlaceholder.tsx
+++ src/components/MultipleSelectPlaceholder/MultipleSelectPlaceholder.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
-import { Theme, useTheme } from '@mui/material/styles';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import "./MultipleSelectPlaceholder.css"
-import { GetSet } from '../../services/SetService';
+import * as React from "react";
+import { Theme, useTheme } from "@mui/material/styles";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import "./MultipleSelectPlaceholder.css";
+import { GetSet } from "../../services/SetService";
+import StandardImageList from "../StandardImageList/StandardImageList";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -19,16 +20,16 @@ const MenuProps = {
 };
 
 const names = [
-  'Set Base',
-  'Van Henry',
-  'April Tucker',
-  'Ralph Hubbard',
-  'Omar Alexander',
-  'Carlos Abbott',
-  'Miriam Wagner',
-  'Bradley Wilkerson',
-  'Virginia Andrews',
-  'Kelly Snyder',
+  "Set Base",
+  "Van Henry",
+  "April Tucker",
+  "Ralph Hubbard",
+  "Omar Alexander",
+  "Carlos Abbott",
+  "Miriam Wagner",
+  "Bradley Wilkerson",
+  "Virginia Andrews",
+  "Kelly Snyder",
 ];
 
 function getStyles(name: string, personName: readonly string[], theme: Theme) {
@@ -43,51 +44,56 @@ function getStyles(name: string, personName: readonly string[], theme: Theme) {
 export default function MultipleSelectPlaceholder() {
   const theme = useTheme();
   const [personName, setPersonName] = React.useState<string[]>([]);
+  const [selectedValue, setSelectedValue] = React.useState<string[]>([]);
 
-  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+  const handleChange = async (event: SelectChangeEvent<typeof personName>) => {
     const {
       target: { value },
     } = event;
     setPersonName(
       // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
+      typeof value === "string" ? value.split(",") : value
     );
-    GetSet("base1");
+    const result = await GetSet("base1");
+    setSelectedValue(result);
   };
 
   return (
-    <div className='multiple-select-placeholder'>
-      <FormControl sx={{ m: 1, width: 300, mt: 3 }}>
-        <Select
-          multiple
-          displayEmpty
-          value={personName}
-          onChange={handleChange}
-          input={<OutlinedInput />}
-          renderValue={(selected) => {
-            if (selected.length === 0) {
-              return <em>Placeholder</em>;
-            }
+    <>
+      <div className="multiple-select-placeholder">
+        <FormControl sx={{ m: 1, width: 300, mt: 3 }}>
+          <Select
+            multiple
+            displayEmpty
+            value={personName}
+            onChange={handleChange}
+            input={<OutlinedInput />}
+            renderValue={(selected) => {
+              if (selected.length === 0) {
+                return <em>Placeholder</em>;
+              }
 
-            return selected.join(', ');
-          }}
-          MenuProps={MenuProps}
-          inputProps={{ 'aria-label': 'Without label' }}
-        >
-          <MenuItem disabled value="">
-            <em>Placeholder</em>
-          </MenuItem>
-          {names.map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, personName, theme)}
-            >
-              {name}
+              return selected.join(", ");
+            }}
+            MenuProps={MenuProps}
+            inputProps={{ "aria-label": "Without label" }}
+          >
+            <MenuItem disabled value="">
+              <em>Placeholder</em>
             </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
+            {names.map((name) => (
+              <MenuItem
+                key={name}
+                value={name}
+                style={getStyles(name, personName, theme)}
+              >
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <StandardImageList selectedValue={selectedValue}></StandardImageList>
+    </>
   );
 }
